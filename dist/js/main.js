d54b0e775eb26551b68be9c47d8b452cf415a83e
Omoniyi Temitayo <temit3684@gmail.com>
2025-05-28 14:18:37 +0100

    New Commit
    Themed-Toggle Animation Added.
    JavaScript Added for Interactivity.

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+const themeToggle = document.querySelector('#toggle');
+const card = document.querySelector('#theme-card');
+
+themeToggle.addEventListener('click', () => {
+    if (card.classList.contains('btn-toggle')) {
+        card.classList.remove('btn-toggle');
+        themeToggle.textContent = "Dark Mode";
+    } else {
+        card.classList.add('btn-toggle');
+        themeToggle.textContent = 'Dark Mode';
+    }
+    }
+);
